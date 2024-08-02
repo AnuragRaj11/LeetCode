3795554c5635337a796c037b5d0c54dec84ff3fd
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2024-08-02 23:09:14 +0530

    Time: 2 ms (75.34%), Space: 53.4 MB (11.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,11 +3,9 @@ class Solution {
         int cand=0;int count=0;
         for(int i=0;i<nums.length;i++){
             if(count==0){
-                cand=nums[i];
-            }
+                cand=nums[i];}
             if(cand==nums[i]){
-                count++;
-            }
+                count++;}
             else{count--;}
         }
         return cand;
