3321c6863c906c0ffe5721007ea4b2d92889a423
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2024-08-09 00:07:42 +0530

    Time: 2 ms (60.34%), Space: 56.8 MB (82.02%) - LeetHub

--- 0775-global-and-local-inversions/0775-global-and-local-inversions.java
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isIdealPermutation(int[] nums) {
         for(int i=0;i<nums.length;i++){
             if(Math.abs(nums[i]-i)>1) return false;
-        } return true;
+        } 
+        return true;
     }
 }
