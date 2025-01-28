0b1820e0d07bf3b245019ba8c2aa53d281b21740
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2025-01-28 21:10:32 +0530

    Time: 223 ms (6.96%), Space: 64.8 MB (5.04%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        return sieve(n);
+    }
+    static int  sieve(int n){
+        if(n<=1) { return 0; }
+        Boolean[] p = new Boolean[n+1];
+        Arrays.fill(p,true);
+        p[0]=false;
+        p[1]=false;
+        for(int i=2;i*i<n;i++){
+            if(p[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    p[j]=false;
+                }
+            }
+        }
+        int c=0;
+        for(int i=2;i<n;i++){
+            if(p[i]){
+                c++;
+            }
+        }
+        return c;
+    }
+}
