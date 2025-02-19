2cbb5b4a82e284d94b5b091bf19f904ed8d31295
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2025-02-19 13:53:44 +0530

    Time: 2 ms (99.65%), Space: 43.4 MB (35.78%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,21 +1,25 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         comb(candidates, target, 0, new ArrayList<>());
         return result;
     }
-    private void comb(int[] candidates, int tar,int ind,List<Integer> curc){
-        if(tar==0){
+
+    private void comb(int[] candidates, int tar, int ind, List<Integer> curc) {
+        if (tar == 0) {
             result.add(new ArrayList<>(curc));
             return;
         }
-        for(int i=ind;i<candidates.length;i++){
-           if(candidates[i]>tar) break;
-           if(i>ind && candidates[i]==candidates[i-1]) continue;
+        for (int i = ind; i < candidates.length; i++) {
+            if (candidates[i] > tar)
+                break;
+            if (i > ind && candidates[i] == candidates[i - 1])
+                continue;
             curc.add(candidates[i]);
-            comb(candidates,tar-candidates[i],i+1,curc);
-            curc.remove(curc.size()-1);
+            comb(candidates, tar - candidates[i], i + 1, curc);
+            curc.remove(curc.size() - 1);
         }
     }
 }
