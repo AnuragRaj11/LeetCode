84ee739bb72e2b3483265225eb875d177ab59d6b
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2025-03-11 22:58:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.java
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -1,59 +1,57 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.lang.Math;
+// Initial Template for Java
 
-class Sorting
-{
-	
-	static void printArray(int arr[],int size)
-	{
-		int i;
-		for(i=0;i<size;i++)
-		System.out.print(arr[i]+" ");
-	    System.out.println();
-	}
-	
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t= sc.nextInt();
-		while(t>0)
-		{
-			int n = sc.nextInt();
-			int a[] = new int[n];
-		
-			for(int i=0;i<n;i++)
-			a[i]= sc.nextInt();
-			
-			 new Solution().insertionSort(a,n);
-			 printArray(a,n);
-			
-		t--;
-		}
-		
-	}
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            new Solution().insertionSort(arr);
+
+            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+
+            // System.out.println();
+
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
 }
+
 // } Driver Code Ends
 
 
-class Solution
-{
-  static void insert(int arr[],int f,int sec)
-  {
-       // Your code here
-       int t=arr[f];
-       arr[f]=arr[sec];
-       arr[sec]=t;
-  }
-  //Function to sort the array using insertion sort algorithm.
-  public void insertionSort(int arr[], int n)
-  {
-      for(int i=0;i<arr.length-1;i++){
-          for(int j=i+1;j>0;j--){
-              if(arr[j]<arr[j-1]){
-                  insert(arr,j,j-1);
-              }
-          }
-      }
-  }
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
