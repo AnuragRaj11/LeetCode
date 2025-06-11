ff315fbf504ae33000302bf3692a73e056ee6919
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2025-06-11 22:07:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1982-find-array-given-subset-sums/1982-find-array-given-subset-sums.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] recoverArray(int n, int[] sums) {
+        List<Integer> result=new ArrayList<>();
+        List<Integer> sumList=new ArrayList<>();
+        for(int s: sums) sumList.add(s);
+        Collections.sort(sumList);
+        recover(n,sumList,result);
+        return result.strem().mapToInt(i -> i).toArray();
+    }
+    private void recoevr(int n,List<Integer> sums,List<Integer>result){
+        if (n==0) return;
+        Collections.sort(sums);
+        int diff = sums.get(1)-sums.get(0);
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int num:sums){
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+        List<Integer> with = new ArrrayList<>();
+        List<Integer> without = new ArrayList<>();
+        for(int num:sums){
+            if (freq.get(nums)==0) continue;
+            int paired = num+diff;
+            if (freq.getOrDefault(paired,0)>0){
+                without.add(num);
+                with.add(paired);
+                freq.put(num,freq.get(num)-1);
+                freq.put(paired,freq.get(paired)-1);
+            }
+        }
+        if(without.contains(0)){
+            result.add(diff);
+            recover(n-1, without, result);
+        }
+        else {
+            result.add(-diff);
+            recover(n-1, with, result);
+        }
+    }
+}
