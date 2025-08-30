36b3fced03c5d2d95d933c816d132facc3e26156
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2025-08-30 17:11:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,11 @@
+SELECT
+    CASE
+        WHEN income < 20000 THEN 'Low Salary'
+        WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+        ELSE 'High Salary'
+    END AS category,
+    COUNT(*) AS accounts_count
+FROM
+    Accounts
+GROUP BY
+    category;
