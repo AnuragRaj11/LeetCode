36d30e12125c8ff918d8d19547b65587c53b2390
Anurag Raj <78250070+AnuragRaj11@users.noreply.github.com>
2026-02-23 19:50:58 +0530

    Time: 6 ms (90.34%), Space: 57.6 MB (92%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> lt=new ArrayList<>();
-        for(int i=0;i<nums.length-1;i++){
+        for(int i=0;i<nums.length;i++){
             int ind=Math.abs(nums[i])-1;
             if(nums[ind]<0){
                 lt.add(Math.abs(nums[i]));
